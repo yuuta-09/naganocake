2a8edd2297300457454fbd2bebce86c5a9d0da77
yuta <vanguardibuki@gmail.com>
2024-02-16 20:01:26 +0900

    ログイン時に顧客のアカウントが有効か判定するメソッドを追加・実装

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -2,6 +2,7 @@
 
 class Public::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
+  before_action :customer_state, only: [:create]
 
   # GET /resource/sign_in
   # def new
@@ -18,10 +19,27 @@ class Public::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  # protected
+  protected
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+
+  def customer_state
+    customer = Customer.find_by(email: params[:customer][:email])
+    # アカウントを取得できなかった場合、このメソッドを終了する
+    return if customer.nil?
+
+    # 取得したアカウントのパスワードと入力されたパスワードが一致していない場合、このメソッドを終了する
+    return unless customer.valid_password?(params[:customer][:password])
+
+    # ユーザが有効の場合はこのメソッドを終了
+    if customer.is_active
+      return
+    else
+      # ユーザが有効でない場合は新規登録画面へ遷移
+      redirect_to new_customer_registration_path
+    end
+  end
 end
