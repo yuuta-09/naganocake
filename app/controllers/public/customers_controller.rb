d44e50e15a2c8e3a3cbc1ec7dbdfc668137fe382
yuta <vanguardibuki@gmail.com>
2024-02-16 18:35:12 +0900

    ログイン中の顧客の情報をコントローラで取得するように修正

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,9 +1,35 @@
 class Public::CustomersController < ApplicationController
   before_action :authenticate_customer!  # Customerコントローラのすべてのアクションは顧客としてログイン済みの時のみつかえる
+  before_action :define_current_customer    # ログイン中の顧客をインスタンス変数に渡す
 
   def show
   end
 
   def edit
+  end
+
+  def update
+    if @customer.update(customer_params)
+      redirect_to customer_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def customer_params
+    keys = [
+      :last_name,        :first_name,
+      :last_name_kana,   :first_name_kana,
+      :postal_code,      :address,
+      :telephone_number, :email
+    ]
+
+    params.require(:customer).permit(keys)
+  end
+
+  def define_current_customer
+    @customer = current_customer
   end
 end
