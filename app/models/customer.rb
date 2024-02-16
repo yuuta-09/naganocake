61295d36a8dab02abafdaf0a6790f7c5995b94c2
yuta <vanguardibuki@gmail.com>
2024-02-16 16:34:10 +0900

    顧客のマイページを作成

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,4 +3,14 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  
+  # フルネームを取得するメソッド
+  def get_fullname(separator="")
+    return self.last_name + separator.to_s + self.first_name
+  end
+
+  def get_fullname_kana(separator="")
+    return self.last_name_kana + separator.to_s + self.first_name_kana
+  end
 end
