12a7b30c93faa8967c6e55096f31284eda47e911
yuta <vanguardibuki@gmail.com>
2024-02-18 01:37:28 +0900

    管理者の商品作成機能の実装

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,13 +1,37 @@
 class Admin::ItemsController < ApplicationController
+  before_action :authenticate_admin!                   # 管理者ログイン済みの場合のみ商品の機能を使用できる
+  before_action :define_genres, only: [:new, :create]  # 予め全てのGenreを取得
+  
   def index
   end
 
   def new
+    @item = Item.new
+    @genres = Genre.all
   end
 
   def show
   end
 
   def edit
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to admin_item_path(@item)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :name, :introduction, :price, :genre_id, :is_active)
+  end
+
+  def define_genres
+    @genres = Genre.all
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,15 @@
 class Item < ApplicationRecord
+  # 画像投稿のための設定
   has_one_attached :image
+
+  # アソシエーションの設定
+  belongs_to :genre
+
+  # バリデーションの設定
+  validates :image,         presence: true
+  validates :name,          presence: true, length: {maximum: 255}
+  validates :introduction,  presence: true, length: {maximum: 255}
+  validates :price,         presence: true
+  validates :genre_id,      presence: true
+  validates :is_active,     presence: true
 end
