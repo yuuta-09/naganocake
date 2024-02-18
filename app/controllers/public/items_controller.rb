18593c5c43f8fd2594a96f67699e01f6d91cd625
yuta-09 <113159750+yuuta-09@users.noreply.github.com>
2024-02-18 21:24:06 +0900

    customer/itemsで販売中の者のみ表示されるように修正

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,15 +1,22 @@
 class Public::ItemsController < ApplicationController
   def index
-    if params[:item_kwg]
-      items = Item.search_by_kwg(params[:item_kwg])
-    else
-      items = Item.all
-    end
+    items = get_active_items
     @items = items.page(params[:page]).per(8)
     @items_count = items.count
   end
 
   def show
     @item = Item.find(params[:id])
+  end
+
+  private
+  def get_active_items
+    if params[:item_kwg]
+      items = Item.search_by_kwg(params[:item_kwg])
+    else
+      items = Item.all
+    end
+
+    return items.where(is_active: true)
   end
 end
