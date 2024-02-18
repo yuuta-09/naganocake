1cc781d5225f1e2b768c8461416c5bf03e821115
yuta <vanguardibuki@gmail.com>
2024-02-18 10:45:43 +0900

    画像はなくてもいいようにバリデーションを変更

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,10 +6,17 @@ class Item < ApplicationRecord
   belongs_to :genre
 
   # バリデーションの設定
-  validates :image,         presence: true
   validates :name,          presence: true, length: {maximum: 255}
   validates :introduction,  presence: true, length: {maximum: 255}
   validates :price,         presence: true
   validates :genre_id,      presence: true
   validates :is_active,     inclusion: [true, false] # presence trueだとfalseの時に空と認識される
+
+  def get_status
+    return is_active ? "販売中" : "販売停止中"
+  end
+
+  def get_status_class
+    return is_active ? "text-success" : "text-secondary"
+  end
 end
