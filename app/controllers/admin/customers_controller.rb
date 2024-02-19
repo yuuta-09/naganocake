57aab54dd9c26828aa0b01369fa4ae22fb463f4a
yuta-09 <113159750+yuuta-09@users.noreply.github.com>
2024-02-20 01:46:54 +0900

    admin/customersの編集機能を実装

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -12,9 +12,32 @@ class Admin::CustomersController < ApplicationController
   def edit
   end
 
+  def update
+    if @customer.update(customer_params)
+      redirect_to admin_customer_path(@customer.id)
+    else
+      render :edit
+    end
+  end
+
   private
 
   def define_customer
     @customer = Customer.find(params[:id])
+  end
+
+  def customer_params
+    keys = [
+      :last_name,
+      :first_name,
+      :last_name_kana,
+      :first_name_kana,
+      :postal_code,
+      :address,
+      :telephone_number,
+      :email,
+      :is_active
+    ]
+    params.require(:customer).permit(keys)
   end
 end
