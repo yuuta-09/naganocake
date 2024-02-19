588c28815ba491bacc3a48ca09a6294615ce58e6
yuta-09 <113159750+yuuta-09@users.noreply.github.com>
2024-02-20 00:47:26 +0900

    admin/customersの詳細ページの実装

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,5 +1,6 @@
 class Admin::CustomersController < ApplicationController
   before_action :authenticate_admin!
+  before_action :define_customer,     except: [:index]
 
   def index
     @customers = Customer.page(params[:page])
@@ -9,5 +10,11 @@ class Admin::CustomersController < ApplicationController
   end
 
   def edit
+  end
+
+  private
+
+  def define_customer
+    @customer = Customer.find(params[:id])
   end
 end
