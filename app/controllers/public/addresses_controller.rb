9be762a7520d5518ee77015c6a809b45eedd750a
yuta-09 <113159750+yuuta-09@users.noreply.github.com>
2024-02-20 04:21:09 +0900

    customer/addressesの新規作成機能を実装

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,7 +1,28 @@
 class Public::AddressesController < ApplicationController
+  before_action :authenticate_customer! # 全てのアクションは顧客としてのログインが必要
+
+
   def index
+    @addresses = current_customer.addresses
+    @address = Address.new
   end
 
   def edit
+  end
+
+  def create
+    @address = Address.new(address_params)
+    @address.customer = current_customer
+    if @address.save 
+      redirect_to(addresses_path)
+    else
+      @addresses = Address.all
+      render(:index)
+    end
+  end
+
+  private
+  def address_params
+    params.require(:address).permit(:postal_code, :address, :name)
   end
 end
--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,8 @@
 class Address < ApplicationRecord
   belongs_to :customer
+
+  # バリデーション
+  validates :postal_code, presence: true, length: {minimum: 4, maximum: 9}
+  validates :address,     presence: true, length: {maximum: 50}
+  validates :name,        presence: true, length: {maximum: 20}
 end
