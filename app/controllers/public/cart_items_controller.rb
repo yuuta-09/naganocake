24891d524478c23f831e77737f646cadd4dd7827
yuta-09 <113159750+yuuta-09@users.noreply.github.com>
2024-02-20 18:29:35 +0900

    customer/cart_itemsのすべての機能に顧客としてログインしていないといけない制限を設定

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,4 +1,6 @@
 class Public::CartItemsController < ApplicationController
+  before_action :authenticate_customer!
+
   def index
     @cart_items = current_customer.cart_items
     @total_payment = CartItem.get_total_payment(@cart_items)
