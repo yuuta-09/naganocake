f40259e9a48c4fa74b74e08a280e41df94a98d59
yuta-09 <113159750+yuuta-09@users.noreply.github.com>
2024-02-20 20:32:48 +0900

    itemモデルのget_imageメソッドの引数をほとんど使用しなかったため引数を削除

--- app/models/item.rb
+++ app/models/item.rb
@@ -33,7 +33,7 @@ class Item < ApplicationRecord
   end
 
   # 画像に関するメソッド
-  def get_image(width=100, height=100)
+  def get_image
     unless image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
       image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
