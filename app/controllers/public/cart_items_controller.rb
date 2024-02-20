3ee53be68c91ffe5de4adba1d09155fb7cb60ceb
yuta-09 <113159750+yuuta-09@users.noreply.github.com>
2024-02-20 16:15:33 +0900

    customer/cart-itemsのカートに追加機能を実装

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,4 +1,21 @@
 class Public::CartItemsController < ApplicationController
   def index
   end
+
+  def create
+    @cart_item = CartItem.new(cart_item_params)
+    @cart_item.customer = current_customer
+    if @cart_item.save
+      redirect_to cart_items_path
+    else
+      @item = Item.find(cart_item_params[:item_id])
+      render "public/items/show"
+    end
+  end
+
+  private
+
+  def cart_item_params
+    params.require(:cart_item).permit(:item_id, :amount)
+  end
 end
