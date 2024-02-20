71facf1746388805dff3fba0f8a0f09a38f05d9d
yuta-09 <113159750+yuuta-09@users.noreply.github.com>
2024-02-20 17:48:29 +0900

    customer/cart-itemsの一覧機能の実装

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,5 +1,7 @@
 class Public::CartItemsController < ApplicationController
   def index
+    @cart_items = current_customer.cart_items
+    @total_payment = CartItem.get_total_payment(@cart_items)
   end
 
   def create
--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -3,4 +3,20 @@ class CartItem < ApplicationRecord
   belongs_to :item
 
   validates :amount, presence: true
+
+  # カート内商品の合計金額を求めるメソッド
+  def get_total_money
+    total_money = (item.price + item.price*0.1) * amount
+    return total_money.floor()
+  end
+
+  # カート内商品のすべての商品の合計金額を求めるメソッド
+  def self.get_total_payment(cart_items)
+    total_payment = 0
+    for cart_item in cart_items do
+      total_payment += cart_item.get_total_money
+    end
+
+    return total_payment
+  end
 end
