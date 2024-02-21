ae784124260c12e08b63b3ac88d2723afc3bc912
yuta-09 <113159750+yuuta-09@users.noreply.github.com>
2024-02-22 01:17:40 +0900

    アプリのdefault_localeを日本に設定

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,7 @@ module Myapp
     # config.eager_load_paths << Rails.root.join("extras")
 
     config.action_view.field_error_proc = Proc.new { |html_tag, instance| html_tag }
+
+    config.i18n.default_locale = :ja
   end
 end
