dc2761f4e7531aad486df4cba48acffb986dc079
yuta-09 <113159750+yuuta-09@users.noreply.github.com>
2024-02-25 21:13:27 +0900

    admin/ordersの注文一覧ページ(管理者トップページ)に管理者としてログインしなければならない制限を追加

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,4 +1,5 @@
 class Admin::HomesController < ApplicationController
+  before_action :authenticate_admin! # 管理者としてログインしなけらば使用できない
   def top
     @orders = Order.includes(:customer)
     @orders = @orders.page(params[:page])
